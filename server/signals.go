40d60e557496b4ebe3761b083f6651fb001f3115
Artyom Kovalevich <kovalevich.a@i20.biz>
2017-10-09 20:18:10 +0700

    Listen signals

--- server/signals.go
+++ server/signals.go
@@ -4,12 +4,14 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+	"os"
 
 	log "github.com/AKovalevich/scrabbler/log/logrus"
 
 )
 
 func (server *Server) configureSignals() {
+	server.signals = make(chan os.Signal, 1)
 	signal.Notify(server.signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1)
 }
 
@@ -17,15 +19,20 @@ func (server *Server) listenSignals() {
 	for {
 		sig := <-server.signals
 		switch sig {
+		case syscall.SIGINT:
+		case syscall.SIGTERM:
+			log.Do.Info("Fast shutdown")
+			break
 		case syscall.SIGUSR1:
-			log.Do.Infof("Signal: ", sig)
+			log.Do.Info("Re-opening log files")
+			break
+		case syscall.SIGQUIT:
+			log.Do.Info("Graceful shutdown")
+			break
+		case syscall.SIGHUP:
+			log.Do.Info("Changing configuration")
+			break
 		default:
-			log.Do.Infof("I have to go... %+v", sig)
-			reqAcceptGraceTimeOut := time.Duration(3)
-			if reqAcceptGraceTimeOut > 0 {
-				log.Do.Infof("Waiting %s for incoming requests to cease", 3)
-				time.Sleep(3)
-			}
 			log.Do.Info("Stopping server gracefully")
 			//server.Stop()
 		}
