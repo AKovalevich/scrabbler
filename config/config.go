182a9347ecab488ab946007577a43766c2939deb
Artyom Kovalevich <kovalevich.a@i20.biz>
2017-10-13 23:15:45 +0700

    Debug

--- config/config.go
+++ config/config.go
@@ -20,18 +20,18 @@ const (
 
 type ScrabblerConfiguration struct {
 	// Main configuration
-	Debug			bool	`toml:"debug" short:"d" description:"Enable debug mode" export:"true"`
-	LogLevel		string	`toml:"log_level" short:"l" description:"Log level" export:"true"`
-	ConfigFilePath	string	`toml:"config_file_path" short:"c" description:"Path to configuration directory, load configuration.toml file in a directory"`
+	Debug			bool	`default:"false" toml:"debug" short:"d" description:"Enable debug mode" export:"true"`
+	LogLevel		string	`default:"info" toml:"log_level" short:"l" description:"Log level" export:"true"`
+	ConfigFilePath	string	`default:"configuration.default.toml" toml:"config_file_path" short:"c" description:"Path to configuration directory, load configuration.toml file in a directory"`
 	// Scrabbler server configuration
-	ServerPort		int		`toml:"server_port" short:"sp" description:"Scrabbler web server port"`
-	ServerHost		string	`toml:"server_host" short:"sd" description:"Scrabbler web server host"`
+	ServerPort		int		`default:"1111" toml:"server_port" short:"sp" description:"Scrabbler web server port"`
+	ServerHost		string	`default:"localhost" toml:"server_host" short:"sd" description:"Scrabbler web server host"`
 	// Web UI configuration
-	WebUI			bool 	`toml:"web_ui" short:"w" description:"Run service with web UI"`
-	WebUIPort		int		`toml:"web_ui_port" short:"wp" description:"Web UI port"`
-	WebUIHost		string	`toml:"web_ui_host" short:"wh" description:"Web UI host"`
+	WebUI			bool 	`default:"true" toml:"web_ui" short:"w" description:"Run service with web UI"`
+	WebUIPort		int		`default:"1112" toml:"web_ui_port" short:"wp" description:"Web UI port"`
+	WebUIHost		string	`default:"localhost" toml:"web_ui_host" short:"wh" description:"Web UI host"`
 	// Shutdown configuration
-	GraceTimeOut 	flaeg.Duration `grace_time_out:"g" description:"Duration to give active requests a chance to finish before Scrabbler stops"`
+	GraceTimeOut 	flaeg.Duration `default:"localhost" grace_time_out:"g" description:"Duration to give active requests a chance to finish before Scrabbler stops"`
 }
 
 func NewScrabblerConfiguration() *ScrabblerConfiguration {
